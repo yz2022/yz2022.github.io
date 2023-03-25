a3bbdd17e6e00e979d45394d9a1918b412e4f335
YiZhou <59765776+yz2022@users.noreply.github.com>
2023-03-25 22:01:40 +0800

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230325.215930';
+const cacheName = 'chirpy-20230325.220125';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
